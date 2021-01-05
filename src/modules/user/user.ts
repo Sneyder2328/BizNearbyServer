d137a6cf0c2a677c976f2989f83aa191689d8493
Kevin <chengkev2000@gmail.com>
2021-01-05 16:49:01 -0400

    changes to user.ts

--- src/modules/user/user.ts
+++ src/modules/user/user.ts
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import {Router} from 'express';
-
-const router = Router();
-
-router.post("/users",(req, resp)=>{
-    resp.send("HEYY!");
-})
-
-export {router as userRouter}
-=======
 import { Router } from 'express';
 import { handleErrorAsync } from '../../middlewares/handleErrorAsync';
 import { signUpUser } from './userService';
@@ -22,4 +11,3 @@ router.post("/users", handleErrorAsync(async (req, res) => {
 }))
 
 export { router as userRouter }
->>>>>>> b75594139ab5fbf89a8b48e91ddc82324cda1f06
