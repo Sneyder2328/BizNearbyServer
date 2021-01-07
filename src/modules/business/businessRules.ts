9293a7a16944b4f20cf528c34c4879c27f133dd3
Andres Alvarez <andres15alvarez@gmail.com>
2021-01-07 10:04:34 -0400

    Fix bugs in businessRules

--- src/modules/business/businessRules.ts
+++ src/modules/business/businessRules.ts
@@ -6,7 +6,7 @@ const trimInside = () => str => str.replace(/\s\s/g, ' ');
 export const newBusinessValidationRules = [
     body('userId').trim().matches(config.regex.uuidV4).withMessage('Invalid Id'),
     body('businessId').trim().matches(config.regex.uuidV4).withMessage('Invalid Id'),
-    body('addresId').trim().matches(config.regex.uuidV4).withMessage('Invalid Id'),
+    body('addressId').trim().matches(config.regex.uuidV4).withMessage('Invalid Id'),
     body('name').customSanitizer(trimInside()).escape().isString().isLength({min: 1, max: 250}).withMessage('Name must be at least 1 character long'),
     body('description').customSanitizer(trimInside()).escape().isString().optional({nullable: true}).isLength({min: 1, max: 500}).withMessage('Description must be at least 1 character long'),
     body('address').customSanitizer(trimInside()).escape().isString().optional({nullable: true}).isLength({min: 1, max: 250}).withMessage('Addres mus be at least 1 character long'),
@@ -18,19 +18,26 @@ export const newBusinessValidationRules = [
     body('cityCode').isNumeric(),
     body('stateCode').isNumeric(),
     body('countryCode').isNumeric(),
-    body('hours').custom(obj => typeof obj.day === 'number' && obj.day <= 7 && typeof obj.openTime === 'string' && obj.openTime.length <= 5 && typeof obj.closeTime === 'string' && obj.closeTime.length <= 5),
-    body('phoneNumber').custom(val => {
-        for(let i = 0; i < val; i++){
-            if(typeof val[i] !== 'string' && val[i].length > 50){
-                return false
+    body('hours').custom(obj => {
+        for(let i = 0; i < obj.length; i++){
+            if(typeof obj[i].day !== 'number' || obj[i].day > 7 || typeof obj[i].openTime !== 'string' || obj[i].openTime.length > 5 || typeof obj[i].closeTime !== 'string' || obj[i].closeTime.length > 5){
+                return false;
+            }
+        }
+        return true;
+    }),
+    body('phoneNumbers').custom(val => {
+        for(let i = 0; i < val.length; i++){
+            if(typeof val[i] !== 'string' || val[i].length > 50){
+                return false;
             }
         }
         return true;
     }).optional({nullable: true}),
     body('categories').custom(val => {
-        for(let i = 0; i<val;i++){
-            if(typeof val[i] !== 'number' && val[i] > 99999){
-                return false
+        for(let i = 0; i<val.length;i++){
+            if(typeof val[i] !== 'number' || val[i] > 99999){
+                return false;
             }
         }
         return true;
