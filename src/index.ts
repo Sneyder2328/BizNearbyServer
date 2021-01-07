6111ff793036fcd93f492f8bfa690a7a68dbeffb
Sneyder <sneyder2328@gmail.com>
2021-01-06 22:42:37 -0400

    Conditionally import module morgan

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import express from 'express'
 import router from './modules/app';
 import compression from 'compression';
-import morgan from 'morgan';
 import knex from './database/knex';
-import {Model} from 'objection';
+import { Model } from 'objection';
 
 export const app = express();
 //DATABASE SETTINGS
@@ -12,13 +11,17 @@ Model.knex(knex);
 //APP SETTINGS
 app.set("PORT", process.env.PORT || 3000);
 
-//APP USE
-app.use(morgan('dev')); //TO VIEW SERVER DATA
-app.use(compression()); //COMPRESS RESPONSES
-app.use(express.json()); //PARSE THE RECEIVING DATA TO JSON
-app.use('/',router); //ALL THE ROUTES
+if (process.env.NODE_ENV == 'development') {
+    const morgan = require('morgan');
+    app.use(morgan('dev')); // enable logging
+}
+
+
+app.use(compression()); // compress responses
+app.use(express.json()); // parse receiving data to json
+app.use('/', router); // add all routes
 
 //APP SERVER
-export const server = app.listen(app.get('PORT'),()=>{
+export const server = app.listen(app.get('PORT'), () => {
     console.log(`app running on port ${app.get("PORT")}`);
 })
