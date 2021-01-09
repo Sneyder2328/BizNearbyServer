c1efb1e5c3f08b98edcf666cb613ab22d46cfbdb
Sneyder <sneyder2328@gmail.com>
2021-01-09 12:35:29 -0400

    Add error handler middleware

--- src/modules/app.ts
+++ src/modules/app.ts
@@ -1,10 +1,12 @@
 import {Router} from 'express';
 import { userRouter } from './user/user';
 import { businessRouter } from './business/business';
+import { errorHandler } from '../middlewares/errorHandler';
 
 const router = Router();
 
 router.use('/', userRouter);
 router.use('/', businessRouter);
+router.use(errorHandler)
 
 export default router;
