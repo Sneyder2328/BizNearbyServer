46d3ba4763ced6924bc8f6a4b33750b0991a9091
Andres Alvarez <andres15alvarez@gmail.com>
2021-01-08 21:20:35 -0400

    Create tests for new business route

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,9 +1,51 @@
 import {User} from '../database/models/User';
+import { Business } from '../database/models/Business';
+import { BusinessAddress } from '../database/models/BusinessAddress';
+import { BusinessCategory } from '../database/models/BusinessCategory';
+import { BusinessHours } from '../database/models/BusinessHours';
+import { BusinessImage } from '../database/models/BusinessImage';
+import { BusinessPhoneNumber } from '../database/models/BusinessPhoneNumber';
+import { UserBusiness } from '../database/models/UserBusiness';
 
 export async function wipeOutDatabase(){
     await User.query().delete();
+    await Business.query().delete();
+    await UserBusiness.query().delete();
+    await BusinessAddress.query().delete();
+    await BusinessCategory.query().delete();
+    await BusinessHours.query().delete();
+    await BusinessImage.query().delete();
+    await BusinessPhoneNumber.query().delete();
 }
 
 export async function CreateUser(user){
     await User.query().insert(user);
+}
+
+export async function CreateBusiness(business){
+    await Business.query().insert(business);
+}
+
+export async function CreateUserBusiness(userBusiness){
+    await UserBusiness.query().insert(userBusiness);
+}
+
+export async function CreateBusinessAddress(businessAddress){
+    await BusinessAddress.query().insert(businessAddress);
+}
+
+export async function CreateBusinessCategory(businessCategory){
+    await BusinessCategory.query().insert(businessCategory);
+}
+
+export async function CreateBusinessHours(businessHours){
+    await BusinessHours.query().insert(businessHours);
+}
+
+export async function CreateBusinessImage(businessImage){
+    await BusinessImage.query().insert(businessImage);
+}
+
+export async function CreateBusinessPhoneNumber(businessPhoneNumber){
+    await BusinessPhoneNumber.query().insert(businessPhoneNumber);
 }
