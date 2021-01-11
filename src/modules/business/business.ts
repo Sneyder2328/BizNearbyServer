1b1cc05dd825b90163bda806c9f59e857ca91d35
Andres Alvarez <andres15alvarez@gmail.com>
2021-01-11 14:39:12 -0400

    Create edit route in business

--- src/modules/business/business.ts
+++ src/modules/business/business.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { validate } from '../../middlewares/validate';
-import { newBusinessValidationRules } from './businessRules';
+import { newBusinessValidationRules, updateBusinessValidationRules } from './businessRules';
 import { handleErrorAsync } from '../../middlewares/handleErrorAsync';
 import { addNewBusiness, updateBusiness } from './businessService';
 import { authenticate } from '../../middlewares/authenticate';
@@ -11,6 +11,12 @@ router.post('/businesses', authenticate, newBusinessValidationRules, validate, h
     const business = req.body;
     const { userId, businessId, addressId, name, description, address, latitude, longitude, cityCode, stateCode, countryCode, bannerUrl, hours, phoneNumbers, categories} = await addNewBusiness(business);
     res.json({ userId, businessId, addressId, name, description, address, latitude, longitude, cityCode, stateCode, countryCode, bannerUrl, hours, phoneNumbers, categories});
+}));
+
+router.put('/businesses/:businessId', authenticate, updateBusinessValidationRules, validate, handleErrorAsync(async (req,res) => {
+    const business = req.body;
+    const businessUpdated = await updateBusiness(business, req.userId, req.params.businessId);
+    res.json(businessUpdated);
 }));
 
 export { router as businessRouter }
