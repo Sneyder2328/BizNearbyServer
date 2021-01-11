e47be8a88c122f754c636a466beff65889014b4b
Sneyder <sneyder2328@gmail.com>
2021-01-11 13:29:19 -0400

    Return data from db in login

--- src/modules/user/userService.ts
+++ src/modules/user/userService.ts
@@ -9,15 +9,11 @@ import { Session } from '../../database/models/Session';
 import { genUUID } from '../../utils/utils';
 import { verifyPassword } from '../../utils/utils';
 
-export const signUpUser = async (user) => {
-    const { id, fullname, email, phoneNumber, thumbnailUrl, password, typeLogin, typeUser } = user;
-    //VALIDATION IN DATABASE
-    const userWithEmail = await User.query().findOne('email', email);
-    if (userWithEmail) throw new AppError(httpCodes.CONFLICT, errors.EMAIL, errors.message.EMAIL_TAKEN);
+export const signUpUser = async ({ id, fullname, email, phoneNumber, thumbnailUrl, password, typeLogin, typeUser }) => {
+    const user = await User.query().findOne('email', email);
+    if (user) throw new AppError(httpCodes.CONFLICT, errors.EMAIL, errors.message.EMAIL_TAKEN);
 
-    //INSERT USER TO DATABASE
     const insertResult = await User.query().insert({ id, fullname, email, phoneNumber, thumbnailUrl, password, typeLogin, typeUser });
-    console.log('insertResult=', insertResult);
 
     const accessToken = genUUID();
     await Session.query().insert({ userId: id, token: accessToken });
@@ -27,20 +23,26 @@ export const signUpUser = async (user) => {
     }
 }
 
-export const logInUser = async (user) => {
-    const userWithEmail = await User.query().findOne('email', user.email);
-    if (!userWithEmail) throw new UserNotFoundError();
+export const logInUser = async ({ email, password, typeLogin }) => {
+    const user = await User.query().findOne('email', email);
+    if (!user) throw new UserNotFoundError();
 
-    if (user.typeLogin === "email") { // Verify password
-        if (!userWithEmail) throw new AuthError();
-        const validPass = await verifyPassword(user.password, userWithEmail.password);
-        if (!validPass) throw new AuthError();
+    // User is using a different typeLogin than expected
+    if (user.typeLogin != typeLogin) { 
+        throw new AuthError()
+    }
+
+    if (typeLogin === "email") { // Verify password
+        const isPasswordCorrect = await verifyPassword(password, user.password);
+        if (!isPasswordCorrect) throw new AuthError();
     }
     else { // Verify third party token
-        
+
     }
-    const { id, fullname, email, thumbnailUrl } = userWithEmail;
-    const uuid = genUUID();
-    await Session.query().insert({ userId: id, token: uuid });
-    return { id, fullname, email, thumbnailUrl, uuid };
+    const accessToken = genUUID();
+    await Session.query().insert({ userId: user.id, token: accessToken });
+    return {
+        accessToken,
+        profile: _.pick(user, ['id', 'fullname', 'email', 'thumbnailUrl', 'typeUser'])
+    };
 }
