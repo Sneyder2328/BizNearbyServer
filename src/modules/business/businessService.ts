00f69d7d49a53e8fe7f9ac6fffb3ccda501b4f46
Andres Alvarez <andres15alvarez@gmail.com>
2021-01-11 13:15:23 -0400

    Create updateBusiness method in businessService

--- src/modules/business/businessService.ts
+++ src/modules/business/businessService.ts
@@ -18,36 +18,94 @@ export const addNewBusiness = async (business) => {
     if(!user) throw new AppError(httpCodes.NOT_FOUND, errors.NOT_FOUND, errors.message.USER_NOT_FOUND);
 
     await Business.query().insert({id: businessId, name, bannerUrl, description});
+
     await UserBusiness.query().insert({userId, businessId});
 
-    const businessAddress = {id: addressId, businessId, address, cityCode, stateCode, countryCode, latitude, longitude};
-    await BusinessAddress.query().insert(businessAddress);
+    await BusinessAddress.query().insert({id: addressId, businessId, address, cityCode, stateCode, countryCode, latitude, longitude});
+    
     const businessCategory = categories.map(async (categoryCode) => {
         await BusinessCategory.query().insert({businessId, categoryCode});
     });
+
     const businessHours = hours.map(async ({day, openTime, closeTime}) => {
         const openTimeInt = parseInt(openTime.replace(':', ''), 10);
         const closeTimeInt = parseInt(closeTime.replace(':',''), 10);
         await BusinessHours.query().insert({businessId, day, openTime: openTimeInt, closeTime: closeTimeInt});
     });
-    if(phoneNumbers.length > 0){
+
+    if(phoneNumbers){
         const businessPhoneNumber = phoneNumbers.map(async (phoneNumber) => {
             await BusinessPhoneNumber.query().insert({businessId, phoneNumber});
         });
         await Promise.all(businessPhoneNumber);
     }
+
     await Promise.all(businessCategory);
     await Promise.all(businessHours);
 
     return {userId, businessId, addressId, name, description, address, latitude, longitude, cityCode, stateCode, countryCode, bannerUrl, hours, phoneNumbers, categories};
 };
 
-export const updateBusiness = async (business) => {
-    const {userId, businessId, addressId, sessionToken, name, description, address, latitude, longitude, cityCode, stateCode, countryCode, bannerUrl, hours, phoneNumbers, categories} = business;
+/**
+ * Verify the user has admin access to the business he's creating/editing the account for
+ * @param userId
+ * @param businessId
+ */
+const verifyUserHasAccessToBusiness = async (userId: string, businessId: string) => {
+    const userBusiness = await UserBusiness.query().findOne({ where: {userId, businessId}});
+    if (!userBusiness) {
+        throw new AppError(httpCodes.FORBIDDEN, 'No permission error', 'You do not have admin permission for the given business');
+    }
+};
 
-    if(!await Business.query().findOne('id', businessId)) throw new AppError(httpCodes.NOT_FOUND, errors.NOT_FOUND, errors.message.);
+export const updateBusiness = async (business) => {
+    const {userId, businessId, addressId, emailNewUser, sessionToken, name, description, address, latitude, longitude, cityCode, stateCode, countryCode, bannerUrl, hours, phoneNumbers, categories} = business;
+
+    if(!await Business.query().findOne('id', businessId)) throw new AppError(httpCodes.NOT_FOUND, errors.NOT_FOUND, errors.message.BUSINESS_NOT_FOUND);
 
     if(!await User.query().findOne('id', userId)) throw new AppError(httpCodes.NOT_FOUND, errors.NOT_FOUND, errors.message.USER_NOT_FOUND);
 
-    if(!await BusinessAddress.query().findOne('id', ))
+    if(!await Session.query().findOne({where: {token: sessionToken, userId}})) throw new AppError(httpCodes.UNAUTHORIZED, errors.CREDENTIAL, errors.message.SESSION_NOT_FOUND);
+
+    verifyUserHasAccessToBusiness(userId, businessId);
+
+    const businessUpdated = await Business.query().patchAndFetchById(businessId, {
+        name, bannerUrl, description
+    });
+
+    const userByEmail = await User.query().findOne('email', emailNewUser);
+    if(!userByEmail) throw new AppError(httpCodes.NOT_FOUND, errors.USER_NOT_FOUND_ERROR, errors.message.USER_NOT_FOUND);
+    else{
+        const userAdded = await UserBusiness.query().insert({userId: userByEmail.id, businessId});
+    }
+    
+    const businessAddressUpdated = await BusinessAddress.query().patchAndFetchById(addressId, {
+        address, cityCode, stateCode, countryCode, latitude, longitude 
+    });
+
+    if(categories){
+        await BusinessCategory.query().delete().whereColumn('businessId', businessId);
+        const businessCategories = categories.map(async (categoryCode) => {
+            await BusinessCategory.query().insert({businessId, categoryCode});
+        });
+        const businessCategoriesAdded = await Promise.all(businessCategories);
+    }
+
+    if(hours){
+        await BusinessHours.query().delete().whereColumn('businessId', businessId);
+        const businessHours = hours.map(async ({day, openTime, closeTime}) => {
+            const openTimeInt = parseInt(openTime.replace(':', ''), 10);
+            const closeTimeInt = parseInt(closeTime.replace(':',''), 10);
+            await BusinessHours.query().insert({businessId, day, openTime: openTimeInt, closeTime: closeTimeInt});
+        });
+        const businessHoursAdded = await Promise.all(businessHours);
+    }
+
+    if(phoneNumbers){
+        await BusinessPhoneNumber.query().delete().whereColumn('businessId', businessId);
+        const businessPhoneNumbers = phoneNumbers.map(async (phoneNumber) => {
+            await BusinessPhoneNumber.query().insert({businessId, phoneNumber});
+        });
+        const businessPhoneNumbersAdded = await Promise.all(businessPhoneNumbers);
+    }
 };
