d4887a49582e872caba238f652e4dd2de7232a2c
Andres Alvarez <andres15alvarez@gmail.com>
2021-01-11 20:22:56 -0400

    Fix missing merge conflict

--- src/modules/app.ts
+++ src/modules/app.ts
@@ -7,13 +7,9 @@ const router = Router();
 
 router.use('/', userRouter);
 router.use('/', businessRouter);
-<<<<<<< HEAD
 router.post('/testing', (req, res) => {
     res.status(200).send("OK");
 })
 router.use(errorHandler)
-=======
-router.use(errorHandler);
->>>>>>> features/business/edit-business
 
 export default router;
