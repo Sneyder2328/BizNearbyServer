4eed916024154d71aa87f3f69b59d0ddfb5e847b
Andres Alvarez <andres15alvarez@gmail.com>
2021-01-12 15:27:14 -0400

    Fix bugs

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -6,8 +6,19 @@ import { BusinessHours } from '../database/models/BusinessHours';
 import { BusinessImage } from '../database/models/BusinessImage';
 import { BusinessPhoneNumber } from '../database/models/BusinessPhoneNumber';
 import { UserBusiness } from '../database/models/UserBusiness';
+import { Session } from '../database/models/Session';
 
 export async function wipeOutDatabase(){
+    const hours = [{
+        day: 1,
+        openTime: 600,
+        closeTime: 1200
+    },
+    {
+        day: 1,
+        openTime: 1300,
+        closeTime: 1830
+    }];
     await User.query().delete();
     await Business.query().delete();
     await UserBusiness.query().delete();
@@ -17,8 +28,19 @@ export async function wipeOutDatabase(){
     await BusinessImage.query().delete();
     await BusinessPhoneNumber.query().delete();
     await User.query().insert({id: 'ebf9b67a-50a4-439b-9af6-25dd7ff4810f', fullname: 'Kevin Cheng', email: 'kevin@gmail.com', password: '12345678', typeLogin: 'email', typeUser: 'normal'});
-}
-
+    await User.query().insert({id: 'f94bcebf-f181-447b-b85f-3c5f36c2a269', fullname: 'Andres Alvarez', email: 'andres@gmail.com', password: '12345678', typeLogin: 'email', typeUser: 'normal'});
+    await Session.query().insert({token: 'fcd84d1f-ee1b-4636-9f61-78dc349f23e5', userId: 'ebf9b67a-50a4-439b-9af6-25dd7ff4810f'});
+    await Business.query().insert({id: "a8bcd05e-4606-4a55-a5dd-002f8516493e", name: "Bodega Mi encanto", description: "My business it's so nice", bannerUrl: "AnURL"});
+    await UserBusiness.query().insert({userId: 'ebf9b67a-50a4-439b-9af6-25dd7ff4810f', businessId: "a8bcd05e-4606-4a55-a5dd-002f8516493e"});
+    await BusinessAddress.query().insert({id: "eee15b20-917f-4d69-a055-e306d938d196", businessId: "a8bcd05e-4606-4a55-a5dd-002f8516493e", });
+    await BusinessCategory.query().insert({businessId: "a8bcd05e-4606-4a55-a5dd-002f8516493e", categoryCode: 1});
+    const businessHours = hours.map(async ({day, openTime, closeTime}) => {
+        await BusinessHours.query().insert({businessId: "a8bcd05e-4606-4a55-a5dd-002f8516493e", day, openTime, closeTime});
+    });
+    await Promise.all(businessHours);
+    await BusinessPhoneNumber.query().insert({businessId: "a8bcd05e-4606-4a55-a5dd-002f8516493e", phoneNumber: "04125568177"});
+ 
+}    
 export async function CreateUser(user){
     delete user?.googleAuth;
     delete user?.facebookAuth;
