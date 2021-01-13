cb7bd2f97a8934111f86dccbdb24a57dc28b5872
Andres Alvarez <andres15alvarez@gmail.com>
2021-01-13 14:11:09 -0400

    Create newReport method

---
+++ src/modules/report/reporteService.ts
@@ -0,0 +1,36 @@
+import { errors } from '../../utils/constants/errors';
+import { httpCodes } from '../../utils/constants/httpResponseCodes';
+import { AppError } from '../../utils/errors/AppError';
+import { Report } from '../../database/models/Report';
+import { Business } from '../../database/models/Business';
+import { User } from '../../database/models/User';
+
+/**
+ * Verify if the user exist in the database
+ * @param userId
+ */
+const verifyUser = async (userId: string) => {
+    if (!await User.query().findById(userId)){
+        throw new AppError(httpCodes.NOT_FOUND, errors.NOT_FOUND, errors.message.USER_NOT_FOUND);
+    } 
+};
+
+/**
+ * Verify if the business exist in the database
+ * @param businessIdId
+ */
+const verifyBusiness = async (businessId: string) => {
+    if (!await Business.query().findById(businessId)){
+        throw new AppError(httpCodes.NOT_FOUND, errors.NOT_FOUND, errors.message.BUSINESS_NOT_FOUND);
+    } 
+};
+
+export const newReport = async ({id, userId, businessId, title, description}) => {
+    verifyUser(userId);
+
+    verifyBusiness(businessId);
+
+    const report = await Report.query().insert({id, userId, businessId, title, description});
+
+    return report;
+};
