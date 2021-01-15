95ef3a0fb2fad16d6d80dbdc623bef4ec6054813
Andres Alvarez <andres15alvarez@gmail.com>
2021-01-15 19:55:26 -0400

    Save jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,6 @@
 module.exports = {
     roots: ['<rootDir>/dist'],
-<<<<<<< HEAD
     testRegex: `user.spec.js$`,
-=======
-    testRegex: `business.spec.js$`,
->>>>>>> features/business/business-by-id
     testEnvironment: "node",
     testPathIgnorePatterns: ['/node_modules/']
 };
