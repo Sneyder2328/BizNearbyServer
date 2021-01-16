1a0e99871caafb6f7b1f4f5e991d849adcbac2c6
Kevin <chengkev2000@gmail.com>
2021-01-16 18:24:20 -0400

    added autocomplete locations functionality

--- src/modules/search/search.ts
+++ src/modules/search/search.ts
@@ -1,16 +1,14 @@
 import { Router } from 'express';
-import { validate } from '../../middlewares/validate';
 import { handleErrorAsync } from '../../middlewares/handleErrorAsync';
 import { endpoints } from '../../utils/constants/endpoints';
-import { AuthError } from '../../utils/errors/AuthError';
 import { authenticate } from '../../middlewares/authenticate';
 import { searchLocations } from './searchService';
 const router = Router();
 
 router.get(endpoints.LOCATION_AUTOCOMPLETE, authenticate, handleErrorAsync(async (req, res) => {
-    const searchPattern = req.query['query'];
-    console.log(searchPattern);
-    const { searchResult } = await searchLocations(searchPattern, req.userId);
+    let searchPattern = req.query['query'];
+    searchPattern = searchPattern.trim();
+    const searchResult = await searchLocations(searchPattern, req.userId);
     res.json(searchResult);
 }))
 
