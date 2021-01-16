745a244d7cb84af2faa1aa95e15e429a516af63c
Andres Alvarez <andres15alvarez@gmail.com>
2021-01-16 12:21:32 -0400

    Add validation for null attributes and empty arrays

--- src/modules/business/businessRules.ts
+++ src/modules/business/businessRules.ts
@@ -1,39 +1,45 @@
 import config from '../../config/config';
 import { body, param } from 'express-validator';
 
-const trimInside = () => str => str.replace(/\s\s/g, ' ');
+const trimInside = () => (str?: String) => str?.replace(/\s\s/g, ' ');
 
 const timeRegex = /^[0|1|2]\d:\d{2}$/;
 
 const businessValidationRules = [
-    body('addressId').trim().matches(config.regex.uuidV4).withMessage('Invalid addressId'),
-    body('name').customSanitizer(trimInside()).escape().isString().isLength({ min: 1, max: 250 }).withMessage('Name must be at least 1 character long'),
+    body('addressId').trim().matches(config.regex.uuidV4).withMessage('Invalid addressId').exists(),
+    body('name').customSanitizer(trimInside()).escape().isString().isLength({ min: 1, max: 250 }).withMessage('Name must be at least 1 character long').exists(),
     body('description').customSanitizer(trimInside()).escape().isString().optional({ nullable: true }).isLength({ min: 1, max: 500 }).withMessage('Description must be at least 1 character long'),
     body('address').customSanitizer(trimInside()).escape().isString().optional({ nullable: true }).isLength({ min: 1, max: 250 }).withMessage('Addres mus be at least 1 character long'),
     body('bannerUrl').trim().isString().escape().optional({ nullable: true }),
-    body('latitude').isNumeric(),
-    body('longitude').isNumeric(),
+    body('latitude').isNumeric().exists(),
+    body('longitude').isNumeric().exists(),
     body('cityCode').isNumeric().optional({ nullable: true }),
     body('stateCode').isNumeric().optional({ nullable: true }),
     body('countryCode').isNumeric().optional({ nullable: true }),
-    body('hours').custom(arr => {
+    body('hours').exists().custom(arr => {
+        if(arr.length === 0){
+            return false;
+        }
         for (let i = 0; i < arr.length; i++) {
             if (typeof arr[i].day !== 'number' || arr[i].day > 7 || !timeRegex.test(arr[i].openTime) || !timeRegex.test(arr[i].closeTime))
                 return false;
         }
         return true;
     }),
-    body('phoneNumbers').custom(val => {
-        for (let i = 0; i < val.length; i++) {
-            if (typeof val[i] !== 'string' || val[i].length > 50) {
+    body('phoneNumbers').custom(arr => {
+        for (let i = 0; i < arr.length; i++) {
+            if (typeof arr[i] !== 'string' || arr[i].length > 50) {
                 return false;
             }
         }
         return true;
     }).optional({ nullable: true }),
-    body('categories').custom(val => {
-        for (let i = 0; i < val.length; i++) {
-            if (typeof val[i] !== 'number' || val[i] > 99999) {
+    body('categories').exists().custom(arr => {
+        if(arr.length === 0){
+            return false;
+        }
+        for (let i = 0; i < arr.length; i++) {
+            if (typeof arr[i] !== 'number' || arr[i] > 99999) {
                 return false;
             }
         }
@@ -42,18 +48,18 @@ const businessValidationRules = [
 ]
 
 export const newBusinessValidationRules = [
-    body('businessId').trim().matches(config.regex.uuidV4).withMessage('Invalid businessId'),
+    body('businessId').trim().matches(config.regex.uuidV4).withMessage('Invalid businessId').exists(),
     ...businessValidationRules
 ];
 
 
 export const updateBusinessValidationRules = [
-    param('businessId').trim().matches(config.regex.uuidV4).withMessage('Invalid businessId'),
+    param('businessId').trim().matches(config.regex.uuidV4).withMessage('Invalid businessId').exists(),
     body('emailNewUser').isEmail().normalizeEmail().withMessage('You must enter a valid email address')
         .isLength({ max: 250 }).withMessage('email too long').optional({nullable: true}),
     ...businessValidationRules
 ];
 
 export const paramBusinessIdValidationRules = [
-    param('businessId').trim().matches(config.regex.uuidV4).withMessage('Invalid businessId')
+    param('businessId').trim().matches(config.regex.uuidV4).withMessage('Invalid businessId').exists()
 ];
